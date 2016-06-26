c31b5f01f7e9c5dc733fdbd23eb26cf87d560d26
Jay Greasley <jay@fliz.co.uk>
2016-06-26 10:17:09 +0100

    comment regarding data retrieval

--- models/wheel/wheel_repo.rb
+++ models/wheel/wheel_repo.rb
@@ -2,6 +2,7 @@ require './models/wheel'
 class WheelRepo
 
 def GetAllWheels
+  # In reality this would go off to DataMApper to get data
   wheel1 = Wheel.new(25)
   wheel2 = Wheel.new(22)
   return [wheel1,wheel2]
